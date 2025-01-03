9e362e5b91a311801ade1a705994d1b82f35b06a
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-04 01:13:26 +0500

    Main File

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 // app.use() ------------------------------------
 
@@ -11,26 +11,48 @@ const port = 3000
 
 
 // app.get() ------------------------------------
-app.get('/', (req, res) => {
-    res.send("You contacted Home path.")
+// app.get('/', (req, res) => {
+    //     res.send("You contacted Home path.")
+// })
+
+// app.get('/contact', (req, res) => {
+    //     res.send("You contacted contact path.")
+    // })
+    
+    // app.get('/about', (req, res) => {
+        //     res.send("You contacted about path.")
+        // })
+        
+        
+// app.post() ------------------------------------
+// app.post('/', (req, res) => {
+    //     res.send("You contacted post root.")
+// })
+
+
+// app.get('*', (req, res) => {
+    //     res.send("This path does not exist.")
+    // })
+    
+
+// Request parameters ------------------------------------  
+// app.get('/:username/:id', (req, res) => {
+//     let {username, id} = req.params
+//     // console.log(username)
+//     res.send(`You contacted ${username} path.`)
+// })
+
+app.get('/:username', (req, res) => {
+        let {username} = req.params
+    // console.log(username)
+    res.send(`You contacted ${username} path.`)
 })
 
-app.get('/contact', (req, res) => {
-    res.send("You contacted contact path.")
-})
 
-app.get('/about', (req, res) => {
-    res.send("You contacted about path.")
-})
-
-
-app.post('/', (req, res) => {
-    res.send("You contacted post root.")
-})
-
-
-app.get('*', (req, res) => {
-    res.send("This path does not exist.")
+// Request parameters ------------------------------------  
+app.get('/search', (req, res) => {
+    console.log(req.query);
+    res.send(`no results`);
 })
 
 
