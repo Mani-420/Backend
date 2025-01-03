da627fe21f95d31de9a3a19de7e5658b6fa8450e
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-04 00:30:00 +0500

    Main File

--- Express/index.js
+++ Express/index.js
@@ -2,14 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// app.use() ------------------------------------
+
 // app.use((req, res) => {
-//     console.log('Mithu Gambler')
+//     console.log("Hello World")
+//     res.send('Hello Handsome')
 // })
 
-app.use((req, res) => {
-    console.log("Hello World")
-    res.send('Hello Handsome')
+
+// app.get() ------------------------------------
+app.get('/', (req, res) => {
+    res.send("You contacted Home path.")
 })
+
+app.get('/contact', (req, res) => {
+    res.send("You contacted contact path.")
+})
+
+app.get('/about', (req, res) => {
+    res.send("You contacted about path.")
+})
+
+
+app.post('/', (req, res) => {
+    res.send("You contacted post root.")
+})
+
+
+app.get('*', (req, res) => {
+    res.send("This path does not exist.")
+})
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
