57ec8370b0624576d7ac8e9b6241b276bac6177d
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-04 22:59:23 +0500

    Main File

--- EJS/index.js
+++ EJS/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const path = require('path');
 
 
+// --------------------------------------------------
+// Setting view engine---
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
+// --------------------------------------
+// Routes
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
 
 
 // --------------------------------------------------
 // Listen port---
 app.listen(PORT, () => {
-    console.log(`Listen on port: ${PORT}`)
+    console.log(`Listen on port: ${PORT}`);
 });
