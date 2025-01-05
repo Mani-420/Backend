03819756b77eae6bce5a347336e2b8a600b4ad57
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-05 22:10:01 +0500

    Main FIle

--- EJS/index.js
+++ EJS/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const PORT = 8080;
 const path = require('path');
 
@@ -19,12 +20,24 @@ app.get("/about", (req, res) => {
     res.render("about.ejs");
 });
 
-app.get("/ig/:username", (req, res) => {
-    const followers = ["Mani", "Hamza", "Farhan"];
+// -------------------------------------------
+// app.get("/ig/:username", (req, res) => {
+//         const followers = ["Mani", "Hamza", "Farhan"];
+//         let {username} = req.params;
+//         res.render("instagram.ejs", {username, followers});
+// });
+    
+
+// -------------------------------------------
+app.get("/insta/:username", (req, res) => {
     let {username} = req.params;
-    res.render("instagram.ejs", {username, followers});
+    const instaData = require("./data.json");
+    let data = instaData[username];
+    console.log(data);
+    res.render("instagram.ejs", {data});
 });
 
+// ------------------------------------------
 app.get("/rolldice", (req, res) => {
     let dicValue = Math.floor(Math.random()*6)+1;
     res.render("rolldice.ejs", {dicValue});
