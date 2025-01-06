743d682cdc07d65b150a49b16c2b074c8310f02a
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-06 23:01:22 +0500

    Necessary OOP

--- Miscellaneous/Frontend/OOP.js
+++ Miscellaneous/Frontend/OOP.js
@@ -7,16 +7,48 @@
 
 
 // Factory Function -----------------------------------
-function personMaker (name, age){
-    const person = {
-        name: name,
-        age: age,
-        talk(){
-            console.log(`My name is: ${name}`);
-        }
-    };
-    return person
+// function personMaker (name, age){
+//     const person = {
+//         name: name,
+//         age: age,
+//         talk(){
+//             console.log(`My name is: ${this.name}`);
+//         }
+//     };
+//     return person
+// }
+
+// let p1 = personMaker("mani", 22);
+// console.log(p1.talk())
+
+
+// New Operator and Constructor -----------------------------------
+// function Person (name, age){
+    //     this.name = name;
+    //     this.age = age;
+    // }
+    
+// Person.prototype.talk = function (){
+    //     console.log(`My name is: ${this.name}`);
+    // }
+    
+    // let p1 = new Person("mani", 22);
+// let p2 = new Person("mithu", 23);
+// console.log(p1.talk())
+// console.log(p2.talk())
+
+
+// Classes -----------------------------------
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`My name is ${this.name}`);
+    }
 }
 
-let p1 = personMaker("mani", 22);
-console.log(p1.talk())
+let p1 = new Person("Mani", 23);
+let p2 = new Person("Gambler", 22);
+console.log(p1.talk());
