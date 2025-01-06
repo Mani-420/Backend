5dd5cd40b0285ea4b3f41044823e0664ede3a1fa
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-06 22:42:37 +0500

    Necessary OOP

--- Miscellaneous/Frontend/OOP.js
+++ Miscellaneous/Frontend/OOP.js
@@ -1,9 +1,22 @@
 // Object -----------------------------------
-const student1 = {
-    name: "mani",
-    age: 22,
-    marks: 95,
-}
+// const student1 = {
+//     name: "mani",
+//     age: 22,
+//     marks: 95,
+// }
 
 
 // Factory Function -----------------------------------
+function personMaker (name, age){
+    const person = {
+        name: name,
+        age: age,
+        talk(){
+            console.log(`My name is: ${name}`);
+        }
+    };
+    return person
+}
+
+let p1 = personMaker("mani", 22);
+console.log(p1.talk())
