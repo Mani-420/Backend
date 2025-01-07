2fb8531536c06fbda768436e37ea188eac38e5f0
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-07 20:53:16 +0500

    Main File

---
+++ REST/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Data for posts
+let posts = [
+    {
+        username: "Mani",
+        content: "I am a Fullstack developer",
+    },
+    {
+        username: "Hamza Habib",
+        content: "I am a Machine Learner",
+    },
+    {
+        username: "Farhan Khalid", 
+        content: "I am a expert backend developer",
+    }
+]
+
+// Requests
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts});
+});
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`App listening to PORT: ${PORT}`);
+});
