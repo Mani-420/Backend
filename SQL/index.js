11440c18f739039d48669c44b43670b5e9650bb4
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-16 18:31:07 +0500

    Main File

--- SQL/index.js
+++ SQL/index.js
@@ -9,8 +9,23 @@ const connection = mysql.createConnection({
   password: 'manitahir420',
 });
 
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+let q = "INSERT INTO practice (id, username, email, password) VALUES ?";
+let data = [];
+for (let i = 1; i <= 100; i++){
+  data.push(getRandomUser());
+}
+
 try{
-  connection.query("SHOW TABLES", (err, result) => {
+  connection.query(q, [data], (err, result) => {
     if (err) throw err;
     console.log(result);
   });
@@ -19,14 +34,3 @@ try{
 }
 
 connection.end();
-
-let getRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-};
-
-// console.log(getRandomUser());
