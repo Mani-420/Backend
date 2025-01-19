f441c3e988826c1fead3a0a1a5bdb2495fec66b9
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-19 18:57:51 +0500

    Main File

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat");
 const method_override = require("method-override");
+const PORT = 8080;
 
 app.set('view engine',"ejs")
 app.set("views",path.join(__dirname,"views"))
@@ -20,51 +21,51 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-const currUser = "Abdul Rehman";
-const currReciever = "Tahir Mehmood";
+// const currUser = "Abdul Rehman";
+// const currReciever = "Tahir Mehmood";
 
-// Index Route 
-
+// Index Route ------------------------------------------------
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
     res.render("index.ejs",{chats});
 });
+// ------------------------------------------------
 
-app.patch("/messages", async (req, res) => {
-  await new Chat({
-    sentFrom: req.body.sentFrom,
-    sentTo: req.body.sentTo,
-    message:req.body.message,
-    createdAt:Date.now()
-  }).save()
-  res.redirect("/")
-});
+// app.patch("/messages", async (req, res) => {
+//   await new Chat({
+//     sentFrom: req.body.sentFrom,
+//     sentTo: req.body.sentTo,
+//     message:req.body.message,
+//     createdAt:Date.now()
+//   }).save()
+//   res.redirect("/")
+// });
 
-app.get("/message/edit", async (req, res) => {
-  const chatObj = await Chat.findById(req.params.id);
-  console.log(chatObj);
-  res.render("index",{messages: await Chat.find({}),user:currUser});
-});
+// app.get("/message/edit", async (req, res) => {
+//   const chatObj = await Chat.findById(req.params.id);
+//   console.log(chatObj);
+//   res.render("index",{messages: await Chat.find({}),user:currUser});
+// });
 
-app.get("/message/:id/edit", async (req, res) => {
-  Chat.findById(req.params.id).then(msg=>{
-    res.render("message",{msg,user:currUser})
-  });
-});
+// app.get("/message/:id/edit", async (req, res) => {
+//   Chat.findById(req.params.id).then(msg=>{
+//     res.render("message",{msg,user:currUser})
+//   });
+// });
 
-app.patch("/message/:id", async (req, res) => {
-  Chat.findByIdAndUpdate((req.params.id),{message:req.body.msg},{new:true}).then(()=>{
-    res.redirect("/")
-  })
-});
+// app.patch("/message/:id", async (req, res) => {
+//   Chat.findByIdAndUpdate((req.params.id),{message:req.body.msg},{new:true}).then(()=>{
+//     res.redirect("/")
+//   })
+// });
 
-app.delete("/message/:id", async (req, res) => {
-  Chat.findByIdAndDelete(req.params.id).then(res=>{
-    console.log(res);
-  })
-  res.redirect("/")
-});
+// app.delete("/message/:id", async (req, res) => {
+//   Chat.findByIdAndDelete(req.params.id).then(res=>{
+//     console.log(res);
+//   })
+//   res.redirect("/")
+// });
 
-app.listen(3001, () => {
-  console.log("app is listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
