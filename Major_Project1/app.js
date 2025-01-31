ba153cb69ec51416296017cc6da5c1d9149af9a4
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-31 23:48:59 +0500

    Main File

--- Major_Project1/app.js
+++ Major_Project1/app.js
@@ -98,7 +98,8 @@ app.all("*", (req, res, next) => {
 // MiddleWares 
 app.use((err, req, res, next) => {
   let {statusCode=500, message="Something went wrong"} = err;
-  res.status(statusCode).send(message);
+  res.render("error.ejs", {msg: message});
+  // res.status(statusCode).send(message);
 })
 
 
