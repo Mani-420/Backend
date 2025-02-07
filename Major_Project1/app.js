33ecdf5fa2187689c2288afed09f31f9335a9f14
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-07 20:26:58 +0500

    Main File

--- Major_Project1/app.js
+++ Major_Project1/app.js
@@ -8,9 +8,13 @@ const PORT = 8080;
 const ExpressError = require('./utils/ExpressError.js');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
-const listings = require('./routes/listing.js');
-const reviews = require('./routes/review.js');
+const listingRouter = require('./routes/listing.js');
+const reviewRouter = require('./routes/review.js');
+const userRouter = require('./routes/user.js');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/Recipe-blog';
 
@@ -49,14 +53,35 @@ app.get('/home', (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 });
 
-app.use('/recipes', listings);
-app.use('/recipes/:id/reviews', reviews);
+app.get('/demouser', async (req, res) => {
+  let fakeUser = new User({
+    username: 'demouser',
+    email: 'demouser@example.com'
+  });
+  let registerUser = await User.register(fakeUser, 'helloDemo');
+  res.send(registerUser);
+});
+
+app.use('/recipes', listingRouter);
+app.use('/recipes/:id/reviews', reviewRouter);
+app.use('/', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new ExpressError(404, 'Page not found'));
