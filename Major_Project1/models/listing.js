8f49986a69803379f9d8bff399e8d047610eddb7
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-11 17:26:09 +0500

    Listing Model

--- Major_Project1/models/listing.js
+++ Major_Project1/models/listing.js
@@ -1,37 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Review = require("./reviews.js");
+const Review = require('./reviews.js');
 
 const listingSchema = new Schema({
   title: {
     type: String,
-    required: true,
+    required: true
   },
   description: String,
   image: {
     type: String,
     default:
-      "https://images.unsplash.com/photo-1498837167922-ddd27525d352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D",
+      'https://images.unsplash.com/photo-1498837167922-ddd27525d352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D',
     set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1498837167922-ddd27525d352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
-        : v,
+      v === ''
+        ? 'https://images.unsplash.com/photo-1498837167922-ddd27525d352?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D'
+        : v
   },
   ingredients: String,
   recipe: String,
   reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: 'Review'
     }
-  ]
-});
-
-listingSchema.post("findOneAndDelete", async (listing) => {
-  if (listing){
-    await Review.deleteMany({_id: {$in: listing.reviews}});
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }
 });
 
-const Listing = mongoose.model("Listing", listingSchema);
+listingSchema.post('findOneAndDelete', async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
 module.exports = Listing;
