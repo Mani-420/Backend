0081d3a0cdb55065aac5e94d8b506d25081dd2fa
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-11 17:25:55 +0500

    Data Initialization

--- Major_Project1/init/index.js
+++ Major_Project1/init/index.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
-const initData = require("./data.js");
-const Listing = require("../models/listing.js");
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../models/listing.js');
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/Recipe-blog";
+const MONGO_URL = 'mongodb://127.0.0.1:27017/Recipe-blog';
 
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log('connected to DB');
   })
   .catch((err) => {
     console.log(err);
@@ -18,8 +18,12 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: '67aa12ac9adfdf7a3c00b7d4'
+  }));
   await Listing.insertMany(initData.data);
-  console.log("Data was initialized");
+  console.log('Data was initialized');
 };
 
 initDB();
