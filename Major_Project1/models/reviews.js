80f7637fd55b94dabd15d7d39959cc4f3e185219
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-13 00:36:46 +0500

    Review Model

--- Major_Project1/models/reviews.js
+++ Major_Project1/models/reviews.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // const User = require("./user.js");
 
@@ -7,16 +7,16 @@ const reviewSchema = new Schema({
   rating: {
     type: Number,
     min: 1,
-    max: 5,
+    max: 5
   },
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now()
   },
-//   author: {
-//     type: Schema.Types.ObjectId,
-//     ref: "User",
-//   },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
-module.exports = mongoose.model("Review", reviewSchema);
+module.exports = mongoose.model('Review', reviewSchema);
