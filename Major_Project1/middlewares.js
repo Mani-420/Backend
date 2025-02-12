07bfbc7e833450cae2d5785f646fa9c9b0e9df9e
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-13 01:09:59 +0500

    MiddleWare

--- Major_Project1/middlewares.js
+++ Major_Project1/middlewares.js
@@ -1,4 +1,5 @@
 const Listing = require('./models/listing');
+const Review = require('./models/reviews');
 const { listingSchema, reviewSchema } = require('./schema.js');
 const ExpressError = require('./utils/ExpressError.js');
 
@@ -23,6 +24,16 @@ module.exports.isOwner = async (req, res, next) => {
   let listing = await Listing.findById(id);
   if (!listing.owner.equals(res.locals.currUser._id)) {
     req.flash('error', 'You are not the owner of this recipe');
+    return res.redirect(`/recipes/${id}`);
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let { id, reviewId } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.author.equals(res.locals.currUser._id)) {
+    req.flash('error', 'You did not create this review');
     return res.redirect(`/recipes/${id}`);
   }
   next();
