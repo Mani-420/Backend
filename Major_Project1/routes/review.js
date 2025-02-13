9b25e7b6abdad94ecb85c5cec3a8af66e9d4158f
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-13 21:37:25 +0500

    Review Route

--- Major_Project1/routes/review.js
+++ Major_Project1/routes/review.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require('../utils/wrapAsync.js');
 const Review = require('../models/reviews.js');
 const Listing = require('../models/listing.js');
+const reviewController = require('../controllers/reviews.js');
 const {
   isLoggedIn,
   validateReview,
@@ -15,16 +16,7 @@ router.post(
   '/',
   validateReview,
   isLoggedIn,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash('success', 'Review added successfully');
-    res.redirect(`/recipes/${listing.id}`);
-  })
+  wrapAsync(reviewController.createReview)
 );
 
 // Delete Reviews Route
@@ -32,13 +24,7 @@ router.delete(
   '/:reviewId',
   isLoggedIn,
   isReviewAuthor,
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Review deleted');
-    res.redirect(`/recipes/${id}`);
-  })
+  wrapAsync(reviewController.destroyReview)
 );
 
 module.exports = router;
