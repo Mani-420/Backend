a30a43e170806a61dc3739666e8c68191a1d83e9
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-13 23:02:25 +0500

    User Route

--- Major_Project1/routes/user.js
+++ Major_Project1/routes/user.js
@@ -1,43 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
 const wrapAsync = require('../utils/wrapAsync.js');
 const passport = require('passport');
 const { saveRedirectUrl } = require('../middlewares.js');
+const userController = require('../controllers/users.js');
 
 // Signup Routes------------------------------------------
-
-router.get('/signup', (req, res) => {
-  res.render('users/signup.ejs');
-});
-
-router.post(
-  '/signup',
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        req.flash('success', 'Welcome to Blog');
-        res.redirect('/recipes');
-      });
-    } catch (err) {
-      req.flash('error', err.message);
-      res.redirect('signup');
-    }
-  })
-);
+router.get('/signup', userController.renderSignUpForm);
+router.post('/signup', wrapAsync(userController.signup));
 
 // Login Routes------------------------------------------
-router.get('/login', (req, res) => {
-  res.render('users/login.ejs');
-});
-
+router.get('/login', userController.renderLogInForm);
 router.post(
   '/login',
   saveRedirectUrl,
@@ -45,21 +18,10 @@ router.post(
     failureRedirect: '/login',
     failureFlash: true
   }),
-  async (req, res) => {
-    req.flash('success', 'Welcome back to Blog');
-    let redirectUrl = res.locals.redirectUrl || '/recipes';
-    res.redirect(redirectUrl);
-  }
+  userController.logIn
 );
 
-router.get('/logout', (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash('success', 'You are logged out');
-    res.redirect('/recipes');
-  });
-});
+// LogOut Routes------------------------------------------
+router.get('/logout', userController.logOut);
 
 module.exports = router;
