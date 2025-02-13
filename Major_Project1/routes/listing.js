a56f37b343191a8eae93fd11477bad61619ac842
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-13 21:25:30 +0500

    Listing Route

--- Major_Project1/routes/listing.js
+++ Major_Project1/routes/listing.js
@@ -4,64 +4,31 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const Listing = require('../models/listing.js');
 const { isLoggedIn, isOwner, validateListing } = require('../middlewares.js');
 
+const listingController = require('../controllers/listings.js');
+
 // All Recipes Route
-router.get(
-  '/',
-  wrapAsync(async (req, res) => {
-    const allRecipes = await Listing.find({});
-    res.render('listings/recipe.ejs', { allRecipes });
-  })
-);
+router.get('/', wrapAsync(listingController.showAllRecipes));
 
 //New Route
-router.get('/new', isLoggedIn, (req, res) => {
-  res.render('listings/new.ejs');
-});
+router.get('/new', isLoggedIn, listingController.renderNewForm);
 
 //Create Route
 router.post(
   '/',
   validateListing,
   isLoggedIn,
-  wrapAsync(async (req, res) => {
-    const newRecipe = new Listing(req.body.listing);
-    newRecipe.owner = req.user._id;
-    await newRecipe.save().catch((err) => console.log(err));
-    req.flash('success', 'Recipe created successfully');
-    res.redirect('/recipes');
-  })
+  wrapAsync(listingController.newRecipe)
 );
 
 // Show Route
-router.get(
-  '/:id',
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const recipes = await Listing.findById(id)
-      .populate({ path: 'reviews', populate: { path: 'author' } })
-      .populate('owner');
-    if (!recipes) {
-      req.flash('error', 'Recipe not found');
-      res.redirect('/recipes');
-    }
-    res.render('listings/show.ejs', { recipes });
-  })
-);
+router.get('/:id', wrapAsync(listingController.showRecipes));
 
 // Edit Route
 router.get(
   '/:id/edit',
   isLoggedIn,
   isOwner,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const recipe = await Listing.findById(id);
-    if (!recipe) {
-      req.flash('error', 'Recipe not found');
-      res.redirect('/recipes');
-    }
-    res.render('listings/edit.ejs', { recipe });
-  })
+  wrapAsync(listingController.renderEditForm)
 );
 
 //Update Route
@@ -70,12 +37,7 @@ router.put(
   isLoggedIn,
   isOwner,
   validateListing,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash('success', 'Recipe updated successfully');
-    res.redirect(`/recipes/${id}`);
-  })
+  wrapAsync(listingController.updateRecipe)
 );
 
 //Delete Route
@@ -83,12 +45,7 @@ router.delete(
   '/:id',
   isLoggedIn,
   isOwner,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndDelete(id);
-    req.flash('success', 'Recipe deleted');
-    res.redirect('/recipes');
-  })
+  wrapAsync(listingController.destroRecipe)
 );
 
 module.exports = router;
