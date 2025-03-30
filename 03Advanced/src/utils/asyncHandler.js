3527202cb5dbcdf36a1740a2ecf095763fa971f3
Mani-420 <tahirabdulrehman46@gmail.com>
2025-03-30 16:55:39 +0500

    Error Handling

---
+++ 03Advanced/src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+  (res, req, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// Another method to handle async errors is to use a try-catch block in the route handler itself
+// and call next(err) in the catch block. This is a more traditional approach but can lead to repetitive code.
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
