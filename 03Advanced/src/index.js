3a07db6f08fc320d1917482df0f99bdf43b4e87d
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-05 15:20:54 +0500

    Main

--- 03Advanced/src/index.js
+++ 03Advanced/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
   path: './.env'
@@ -7,8 +8,8 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    application.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
     });
   })
   .catch((error) => {
