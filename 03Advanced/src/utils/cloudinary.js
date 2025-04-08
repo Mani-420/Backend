1bf931ea660182d13f6ed46fd5515de48cc82d32
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-08 23:17:24 +0500

    Cloudinary File

--- 03Advanced/src/utils/cloudinary.js
+++ 03Advanced/src/utils/cloudinary.js
@@ -1,11 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { fs } from 'fs';
+import fs from 'fs';
 
-// Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -15,9 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: 'auto'
     });
+    // file has been uploaded successfull
     //console.log("file is uploaded on cloudinary ", response.url);
-
-    // If file has been uploaded successfully, then remove the locally saved temporary file
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
