978e171e7337e27a5b07d830a4a726a29c70978b
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-08 23:19:09 +0500

    Error File

--- 03Advanced/src/utils/apiError.js
+++ 03Advanced/src/utils/apiError.js
@@ -8,7 +8,7 @@ class ApiError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.message = message;
-    this.success = success;
+    this.success = false;
     this.data = null;
     this.errors = errors;
     // this.stack = stack;
