e2f8926476e7cc53e4641eca778d3a9898d6ef3f
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-12 00:37:54 +0500

    User Controller

--- 03Advanced/src/controllers/user.controller.js
+++ 03Advanced/src/controllers/user.controller.js
@@ -9,6 +9,11 @@ generateAccessAndRefreshTokens = async (userId) => {
     const user = await User.findById(userId);
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
   } catch (error) {
     throw new ApiError(
       500,
@@ -112,6 +117,39 @@ const loginUser = asyncHandler(async (req, res) => {
   if (!isPasswordValid) {
     throw new ApiError(401, 'Invalid Credentials');
   }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    '-password -refreshToken'
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true
+  };
+
+  return res
+    .status(200)
+    .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken
+        },
+        'User logged in successfully'
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  
 });
 
 export { registerUser, loginUser };
