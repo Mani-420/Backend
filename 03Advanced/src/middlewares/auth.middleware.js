4b5e6891fd04338b6d50547ffc50b803f1edf9ad
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-12 18:37:12 +0500

    Auth Middleware

--- 03Advanced/src/middlewares/auth.middleware.js
+++ 03Advanced/src/middlewares/auth.middleware.js
@@ -1,21 +1,23 @@
-import { asyncHandler } from '../utils/asyncHandler';
-import { ApiError } from '../utils/ApiError';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
 import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model';
+import { User } from '../models/user.model.js';
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies.accessToken ||
-      req.headers('Authorization')?.replace('Bearer ', '');
+      req.header('Authorization')?.replace('Bearer ', '');
+
+    console.log('Token received:', token);
 
     if (!token) {
       throw new ApiError(401, 'Unauthorized access');
     }
 
-    const decodedToken = jwt.verify(token, process.env.JWT.SECRET);
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    const user = await User.findById(decodedToken?._id).select(
+    const user = await User.findById(decodedToken?.id).select(
       '-password -refreshToken'
     );
 
@@ -26,6 +28,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     req.user = user; // Attach user to request object
     next();
   } catch (error) {
-    throw new ApiError(401, 'Invalid Access');
+    console.error('JWT Error:', error.message);
+    throw new ApiError(401, 'Invalid access token');
   }
 });
