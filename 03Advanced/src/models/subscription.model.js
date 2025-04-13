828d27acaa8ef3274dbfd0a1660dbd9a19d389c5
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-13 15:51:06 +0500

    Subscription Model

--- 03Advanced/src/models/subscription.model.js
+++ 03Advanced/src/models/subscription.model.js
@@ -3,11 +3,11 @@ import mongoose, { Schema } from 'mongoose';
 const subscriptionSchema = new Schema(
   {
     subscriber: {
-      type: Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId, // the one who is subscribing
       ref: 'User'
     },
     channel: {
-      type: Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId, // the one who gets subscribed
       ref: 'User'
     }
   },
