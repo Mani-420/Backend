edac0fe75cfb84065b26b544c8c7111b28538cf6
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-14 23:49:32 +0500

    User Controller

--- 03Advanced/src/controllers/user.controller.js
+++ 03Advanced/src/controllers/user.controller.js
@@ -400,6 +400,50 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, channel[0], 'Channel fetched successfully'));
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const { user } = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: 'watchHistory',
+        foreignField: '_id',
+        as: 'watchHistory',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'owner',
+              foreignField: '_id',
+              as: 'owner',
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: '$owner'
+              }
+            }
+          }
+        ]
+      }
+    }
+  ]);
+});
+
 export {
   registerUser,
   loginUser,
