5ee00b52f3cfd2f48b95d6506ecd4b014fac2979
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-16 15:44:35 +0500

    User Controller

--- 03Advanced/src/controllers/user.controller.js
+++ 03Advanced/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -153,7 +154,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: null }
+      $unset: { refreshToken: 1 }
     },
     {
       new: true
@@ -401,7 +402,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 });
 
 const getWatchHistory = asyncHandler(async (req, res) => {
-  const { user } = await User.aggregate([
+  const user = await User.aggregate([
     {
       $match: {
         _id: new mongoose.Types.ObjectId(req.user._id)
